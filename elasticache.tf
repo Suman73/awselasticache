6e78aea9de32b862373c6e8dd63c4c4144112c92
Suman Pramanik <40729668+Suman73@users.noreply.github.com>
2023-08-07 12:39:40 +0530

    Update elasticache.tf

--- elasticache.tf
+++ elasticache.tf
@@ -35,7 +35,7 @@ resource "aws_elasticache_replication_group" "example" {
   replication_group_id        = "tf-rep-group-1"
   description                 = "example description"
   node_type                   = "cache.t2.micro"
-  num_cache_clusters          = 1
+  num_cache_clusters          = 2
   parameter_group_name        = "default.redis3.2"
   port                        = 6379
 }
