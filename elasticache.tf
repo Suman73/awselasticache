ca55a8459e56953a70e82edc3700144b6573de60
Suman Pramanik <40729668+Suman73@users.noreply.github.com>
2023-08-07 10:58:43 +0530

    Update elasticache.tf

--- elasticache.tf
+++ elasticache.tf
@@ -26,11 +26,11 @@ resource "aws_elasticache_replication_group" "default" {
   replication_group_id          = "${var.cluster_id}"
   replication_group_description = "Redis cluster for Hashicorp ElastiCache example"
 
-  node_type            = "cache.m4.large"
+  node_type            = "cache.t2.micro"
   port                 = 6379
   parameter_group_name = "default.redis3.2.cluster.on"
 
-  snapshot_retention_limit = 5
+  snapshot_retention_limit = 1
   snapshot_window          = "00:00-05:00"
 
   subnet_group_name          = "${aws_elasticache_subnet_group.default.name}"
