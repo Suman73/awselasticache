c5b30bf31d91f8ab2b03f659d133dd78da20c677
Suman Pramanik <40729668+Suman73@users.noreply.github.com>
2023-08-07 11:58:29 +0530

    Update elasticache.tf

--- elasticache.tf
+++ elasticache.tf
@@ -26,23 +26,14 @@ resource "aws_elasticache_subnet_group" "default" {
   name       = "my-elasticache-subnet-group"
   subnet_ids = ["subnet-040e75a6dbdbe0c2e", "subnet-012b07d11ce5a65db"]  # Replace with the actual subnet IDs in your VPC
 }
-
 resource "aws_elasticache_replication_group" "default" {
-  replication_group_id          = var.cluster_id
+  replication_group_id   = var.cluster_id
+  automatic_failover_enabled = true
   replication_group_description = "Redis cluster for Hashicorp ElastiCache example"
 
-  node_type            = "cache.t2.micro"
-  port                 = 6379
-  parameter_group_name = "default.redis3.2.cluster.on"
+  # Other configurations for the replication group...
 
-  snapshot_retention_limit = 1
-  snapshot_window          = "00:00-05:00"
+  subnet_group_name = aws_elasticache_subnet_group.default.name
 
-  subnet_group_name          = aws_elasticache_subnet_group.default.name
-  automatic_failover_enabled = true
-
-  cluster_mode {
-    replicas_per_node_group = 1
-    num_node_groups         = var.node_groups
-  }
+  # More configurations for the replication group...
 }
