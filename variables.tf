dd7968c2f1b326fbc4328cfc96e68463869c9ff0
Suman Pramanik <40729668+Suman73@users.noreply.github.com>
2023-08-07 10:44:28 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,27 @@
+variable "vpc_cidr_block" {
+  description = "The top-level CIDR block for the VPC."
+  default     = "172.31.0.0/16"
+}
 
+variable "cidr_blocks" {
+  description = "The CIDR blocks to create the workstations in."
+  default     = ["172.31.32.0/20", "172.31.0.0/20"]
+}
+
+variable "namespace" {
+  description = "Default namespace"
+}
+
+variable "cluster_id" {
+  description = "Id to assign the new cluster"
+}
+
+variable "public_key_path" {
+  description = "Path to public key for ssh access"
+  default     = "~/.ssh/id_rsa.pub"
+}
+
+variable "node_groups" {
+  description = "Number of nodes groups to create in the cluster"
+  default     = 3
+}
