8e373bb5bc75d9fce555d7cb0c083405c5d41e95
Suman Pramanik <40729668+Suman73@users.noreply.github.com>
2023-08-07 11:26:55 +0530

    Update elasticache.tf

--- elasticache.tf
+++ elasticache.tf
@@ -22,13 +22,8 @@ resource "aws_security_group" "default" {
   }
 }
 
-resource "aws_elasticache_subnet_group" "default" {
-  name       = "${var.namespace}-cache-subnet"
-  subnet_ids = ["${aws_subnet.default.*.id}"]
-}
-
 resource "aws_elasticache_replication_group" "default" {
-  replication_group_id          = "${var.cluster_id}"
+  replication_group_id          = var.cluster_id
   replication_group_description = "Redis cluster for Hashicorp ElastiCache example"
 
   node_type            = "cache.t2.micro"
@@ -38,11 +33,11 @@ resource "aws_elasticache_replication_group" "default" {
   snapshot_retention_limit = 1
   snapshot_window          = "00:00-05:00"
 
-  subnet_group_name          = "${aws_elasticache_subnet_group.default.name}"
+  subnet_group_name          = aws_elasticache_subnet_group.default.name
   automatic_failover_enabled = true
 
   cluster_mode {
     replicas_per_node_group = 1
-    num_node_groups         = "${var.node_groups}"
+    num_node_groups         = var.node_groups
   }
 }
