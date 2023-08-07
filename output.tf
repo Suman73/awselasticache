f9176eb4dc76aa9e6e63b2eedb4f7ee18004d3c3
Suman Pramanik <40729668+Suman73@users.noreply.github.com>
2023-08-07 12:36:18 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,6 +1,6 @@
-output "configuration_endpoint_address" {
-  value = "${aws_elasticache_replication_group.default.configuration_endpoint_address}"
-}
+#output "configuration_endpoint_address" {
+ # value = "${aws_elasticache_replication_group.default.configuration_endpoint_address}"
+#}
 
 #output "ssh_host" {
 # value = "${aws_instance.ssh_host.public_ip}"
